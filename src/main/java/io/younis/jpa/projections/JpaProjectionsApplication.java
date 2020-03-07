042afca6078d71a86baa9ddbbdadbf91f0c9523d
Younis Shah <younisashah@gmail.com>
2020-03-07 23:53:29 +0700

    feat: enables JPA Auditing. adds app config

--- src/main/java/io/younis/jpa/projections/JpaProjectionsApplication.java
+++ src/main/java/io/younis/jpa/projections/JpaProjectionsApplication.java
@@ -2,8 +2,10 @@ package io.younis.jpa.projections;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class JpaProjectionsApplication {
 
     public static void main(String[] args) {
