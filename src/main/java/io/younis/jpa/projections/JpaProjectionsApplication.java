a6dcb4f1a6998f7fbc9871d8cbc1e806799f12db
Younis Shah <younisashah@gmail.com>
2020-03-07 22:15:37 +0700

    feat: up

---
+++ src/main/java/io/younis/jpa/projections/JpaProjectionsApplication.java
@@ -0,0 +1,13 @@
+package io.younis.jpa.projections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaProjectionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaProjectionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/younis/jpa/projections/JpaProjectionsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.younis.jpa.projections;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaProjectionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
